302a39ea8082de05a74478051ffbc489b09b9ac4
dududueedu <eduardooffside@gmail.com>
2025-04-14 22:28:08 -0300

    add pages: page adapter

---
+++ pages/adapter/adapter.js
@@ -0,0 +1,73 @@
+// Burger menus
+document.addEventListener('DOMContentLoaded', function() {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    const opClose = document.querySelectorAll('.op-close');
+    if (opClose.length) {
+        for (var i = 0; i < opClose.length; i++) {
+            opClose[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    function checkOpen(){
+        const data = new Date();
+        return data.getHours() >= 8 && data.getHours() < 18;
+    }
+
+    function daySunday(){ //checking sunday
+        const data = new Date();
+        return data.getDay() == 0;
+    }
+
+    // tests
+    // When the user scrolls down 50px from the top of the document, resize the header's font size
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+            document.getElementById("nav-adapter").style.background = "#18181b";
+        }else{
+            document.getElementById("nav-adapter").style.background = "transparent";
+        }
+    }
+});
